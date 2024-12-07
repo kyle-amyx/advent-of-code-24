0534cc306fee1f682a5f2f04ba9656007752fc6c
Kyle Amyx <kyle@iheartjane.com>
2024-12-07 12:42:34 -0500

    day 1 part 2 solved

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,29 @@
 fun main() {
 
-    // Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
-    fun part1(input: List<String>): Int {
-        val summedElfCalories = input.joinToString("\n")
+    fun summedElfCalories(input: List<String>): List<Int> {
+        return input.joinToString("\n")
             .split("\n\n")
             .map { elf ->
                 elf.lines().sumOf {  itemCalories ->
                     itemCalories.toIntOrNull() ?: 0
                 }
             }
+    }
 
-        return summedElfCalories.max()
+    // Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+    fun part1(input: List<String>): Int {
+        return summedElfCalories(input).max()
     }
 
 
+    // Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
     fun part2(input: List<String>): Int {
-        return input.size
+        val elfs = summedElfCalories(input)
+        val top3Elfs = elfs.sortedDescending().take(3)
+        top3Elfs.forEachIndexed { idx, elfTotalCals ->
+            println("Elf #$idx total calories: $elfTotalCals")
+        }
+        return top3Elfs.sum()
     }
 
     // Test if implementation meets criteria from the description, like:
@@ -28,5 +36,5 @@ fun main() {
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
-    //part2(input).println()
+    part2(input).println()
 }
